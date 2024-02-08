88012f08a757c409f934941e265f54b26abcf7f0
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-02-08 21:18:09 +0300

    added a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main () {
+    var fullname = "Nancy Murangira"
+    var age = 25
+    var phonenumber = "0787948672"
+    var kg = 50
+    var Kenyan = false
+    println(fullname)
+    println(age)
+    println(phonenumber)
+    println(kg)
+    println(Kenyan)
+}
+
